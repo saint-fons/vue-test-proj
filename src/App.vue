7ab57579ff0f390f66a6ca9dd306662e18356d5c
Ivan Zhuravlev <i.zhuravlev.wok@gmail.com>
2022-02-17 09:56:19 +0300

    likes, dislikes added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <div>
+      <button v-on:click="addLike">Like</button>
+      <button v-on:click="addDislikes">Dislikes</button>
+    </div>
+    <div>
+      Likes count: <strong>{{ likes }}</strong>
+    </div>
+    <div>
+      Dislikes count: <strong>{{ dislikes }}</strong>
+    </div>
   </div>
-  <router-view/>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislikes() {
+      this.dislikes += 1;
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
